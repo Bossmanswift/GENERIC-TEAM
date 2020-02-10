34d3c05b5712bba9a30b0372cbbb664a62d33cb2
Bossmanswift <olaoluwaseyi81@gmail.com>
2020-02-09 19:37:49 -0600

    add methods, implement user command line interface

--- src/com/generic/main/Main.java
+++ src/com/generic/main/Main.java
@@ -1,140 +1,276 @@
 package com.generic.main;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
 
 import com.generic.model.FreightType;
 import com.generic.model.Shipment;
 import com.generic.model.Warehouse;
 import com.generic.tracker.WarehouseTracker;
+import com.sun.xml.internal.ws.api.server.LazyMOMProvider.Scope;
 
 /**
+ * Entry Point
  * 
- * @author Justin Caughlan, Seyi Ola
+ * To import run the command:
+ * import example.json
+ * 
+ * @author GENERIC TEAM
  *
  */
-
 public class Main {
 	
-	public static WarehouseTracker warehouseTracker = WarehouseTracker.getInstance();
+	public static WarehouseTracker warehouseTracker;
+	
+	public Main() {
+		warehouseTracker = WarehouseTracker.getInstance();
+		
+		System.out.println("Available Commands:");
+		System.out.println("1. import <json_file>");
+		System.out.println("2. export <warehouse_id>");
+		System.out.println("3. print <warehouse_id>");
+		System.out.println("4. print*");
+		System.out.println("5. enablef <warehouse_id>");
+		System.out.println("6. disablef <warehouse_id>");
+		System.out.println("7. add <warehouse_id>");
+		System.out.println("8. exit");
 
+		System.out.println();
+
+	}
 
 	public static void main(String[] args) {
+		Main app = new Main();
+		Scanner in = new Scanner(System.in);
 		
-		// For Testing will be removed, as a user is expected
-		// to pass the path to Json file they would
-		// like to read
-		File jsonfile = new File("resource/example.json");
-		String jsonfilePath = jsonfile.getAbsolutePath();
-		parseJson(jsonfilePath);
-		
-		// Export to a warehouse Json file. Right
-		// now this will export to the project folder,
-		// although not stated in the specification, we
-		// should have a feature that allows user
-		// specify a destination path for their file.
-		warehouseTracker.exportWarehouseToJSON(15566);
-		
-		/*
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Import (I) or Export (E) Warehouse Contents");
-		String response = scanner.nextLine();
-		if(response.equalsIgnoreCase("I")) {
-			//Call JSON Parser
+		loop:
+		while(true) {
+			System.out.print(">> ");
+			if (!in.hasNextLine())
+				break;
+				
+			String[] arg = in.nextLine().split(" ");
+			String command = arg[0];
+			
+			
+			
+			switch(command.toLowerCase()) {		
+				case "import":
+					
+					String file = arg[1];
+					try {
+						System.out.println("Importing "+ file +"...");
+						app.parseJson(new File("resource/"+file).getAbsolutePath());
+						System.out.println("Importing complete!");
+					} catch (IOException | ParseException | ArrayIndexOutOfBoundsException e) {
+						System.out.println("System can not read the file!");
+					}
+					break;
+				
+				case "export":					
+					
+					try {
+						int mWarehouseID = Integer.parseInt(arg[1]);
+						if (!warehouseTracker.warehouseExists(mWarehouseID)) {
+							System.out.println("** Warehouse with ID " + mWarehouseID + " does not exist");
+							break;
+						}
+						warehouseTracker.exportWarehouseToJSON(mWarehouseID);
+					} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+						System.out.println("** Please input an integer");
+					}
+					break;
+				
+				case "print":
+					
+					try {
+						int warehouseID = Integer.parseInt(arg[1]);
+						warehouseTracker.printWarehouseDetails(warehouseID);
+					} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+						System.out.println("** Please input a warehouse_id");
+					}
+					break;
+				
+				case "print*":
+					if (!warehouseTracker.isEmpty()) {
+						warehouseTracker.printAll();
+					}else {
+						System.out.println("** No warehouses avaliable, please import data first");
+					}
+					break;
+					
+				case "enablef":
+					// TODO: write to enable freight on a specified warehouse id
+				case "disablef":
+					// TODO: write to disable freight on a specified warehouse id
+					
+				case "add":
+					try {
+						int warehouseID = Integer.parseInt(arg[1]);
+						if (!warehouseTracker.isEmpty()) {
+							
+							if (!warehouseTracker.warehouseExists(warehouseID)){
+								System.out.println("** Warehouse with ID #" + warehouseID + " does not exist");	
+							}else {
+								if (!app.addShipmentOp(warehouseID)) {
+									System.out.println("** Shipment could not be added because"
+										     		   + " freight has ended for this warehouse");
+								}	
+							}
+						}else {
+							System.out.println("** No warehouses with avaliable, please import data first");
+						}
+					} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+						System.out.println("** Please input a warehouse_id");
+					}
+					
+					
+					
+					
+					
+					break;
+					
+				case "exit":
+					break loop;
+				default:
+					System.out.println("** Invalid Command!");
+					continue;
+			}
 		}
-		*/
+		System.out.println("Goodbye!");
 	}
 	
+	public boolean addShipmentOp(int warehouseID) {
 		
+        Scanner sc = new Scanner(System.in);
+        
+        boolean added = false;
+		
+        String option = "";
+        
+		String shipmentID;
+		FreightType freight;
+		long receiptDate;
+		double weight;
+		
+		
+		
+		do {
+		
+	        System.out.print("Enter shipmentID(i.e. iafr4, 15566) -> ");
+	        while (!sc.hasNext()) {
+	        	System.out.print("Please enter a valid shipmentID(i.e. iafr4, 85545) -> ");
+	        	sc.next();
+	        }
+	        shipmentID = sc.next();
+	       
+	        System.out.print("Enter shipment_method(i.e air, truck, ship, rail) -> ");
+	        
+	        //TODO: Validate input for freight type
+
+
+	        System.out.print("Enter weight(i.e. 84.71, 321) -> ");
+	        while (!sc.hasNextDouble()) {
+	        	System.out.print("Please enter a valid weight(i.e. 84.71, 321) -> ");
+	        	sc.next();
+	        }
+	        weight = sc.nextDouble();
+	        
+	        
+	        System.out.print("Enter receipt_date(i.e 1515354694451) -> ");
+	        while (!sc.hasNextLong()) {
+	        	System.out.print("Please enter a valid receipt_date(i.e 1515354694451) -> ");
+	        	sc.next();
+	        }
+	        receiptDate = sc.nextLong();
+	   
+	        // build a shipment
+	     	Shipment shipment = new Shipment.Builder()
+	     				    				.id(shipmentID)
+	     				    				//.type(FreightType.valueOf(frei))
+	     				    				.weight(weight)
+	     				    				.date(receiptDate)
+	     				    				.build();
+	        
+			// add the shipment to the warehouse
+	        added = warehouseTracker.addShipment(warehouseID, shipment);
+	        
+	        if (added) {
+	        	System.out.println("Incoming shipment has been sucessfully added");
+	        	System.out.print("Would you like to add another shipment to warehouse_" + warehouseID + "?(Y/N)");
+		        while (!sc.hasNext() || !sc.next().equalsIgnoreCase("Y") || !sc.next().equalsIgnoreCase("N")) {
+		        	System.out.print("Please enter ('Y/N')");
+		        	sc.next();
+		        }
+		        option = sc.next();
+			}
+	        
+	        sc.close();
+			
+		} while (option.equalsIgnoreCase("Y") && added);
+		return added; 
+	}
+	
+	
 	/**
 	 * Reads a file that is in JSON format containing
 	 * various shipment information.
 	 * @param filepath the path of JSON file 
 	 */
-	
 	@SuppressWarnings("unchecked")
-	public static void parseJson(String filepath)
-	{
-		
+	public void parseJson(String filepath) throws IOException, ParseException {
 		JSONParser jsonParser = new JSONParser();
-		
-		try (FileReader reader = new FileReader(filepath))
-        {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-             
-            JSONObject jsonFile = (JSONObject) obj;
-    
-            JSONArray warehouseContents = (JSONArray) jsonFile.get("warehouse_contents");
-            
-            //prints out the json array for testing, will be removed
-            System.out.println(warehouseContents);
+		FileReader reader = new FileReader(filepath);
 
-            //Iterate over warehouse array
-            warehouseContents.forEach(shipmentObject -> parseWarehouseContentsToObjects((JSONObject) shipmentObject));
- 
-        } catch (FileNotFoundException e) {
-        	throw new IllegalArgumentException(e.getMessage());
-        } catch (IOException e) {
-        	throw new IllegalArgumentException(e.getMessage());
-        } catch (ParseException e) {
-        	throw new IllegalArgumentException(e.getMessage());
-        }
+		JSONObject jsonFile = (JSONObject) jsonParser.parse(reader);
+		JSONArray warehouseContents = (JSONArray) jsonFile.get("warehouse_contents");
+		warehouseContents.forEach(shipmentObject -> parseWarehouseContentsToObjects((JSONObject) shipmentObject));
+		
+		reader.close();
 	}
-	
-	
+
 	/**
 	 * Parses and assigns shipment 
 	 * object for each warehouse
 	 * @param shipmentObject shipment object in json
 	 */
-	
-	
-	private static void parseWarehouseContentsToObjects(JSONObject shipmentObject)
-	{
-		// Get warehouse tracker instance
-		//WarehouseTracker warehouseTracker = WarehouseTracker.getInstance();
+	private void parseWarehouseContentsToObjects(JSONObject shipmentObject) {
+
+		String warehouseString = (String)shipmentObject.get("warehouse_id");
+		int warehouseID = Integer.parseInt(warehouseString);
 		
-		
-		// 	Get all fields from JSON file and assign 
-		int warehouseID = Integer.parseInt((String)shipmentObject.get("warehouse_id"));
+		// create warehouse
+		Warehouse warehouse = new Warehouse(warehouseID);
+
+		String shipmentID = (String) shipmentObject.get("shipment_id");
 		FreightType freight = FreightType.valueOf((String)shipmentObject.get("shipment_method").toString().toUpperCase());
-
+		Number weight = (Number) shipmentObject.get("weight");
+		Number receiptDate = (Number) shipmentObject.get("receipt_date");
 		
-		// SOME SLICK STUFF
-		// GOING ON HERE. THIS CHECKS IF THE RETURN VALUE OF 
-		// get("shipment_id") is null due what may be(or not be)
-		// a typo in the json file(Shipment_id), then returns
-		// get ("Shipment_id") instead
-		// 
-		String shipmentID = ((shipmentObject.get("shipment_id") != null) ? 
-							(String)shipmentObject.get("shipment_id") :
-							(String) shipmentObject.get("Shipment_id"));
-		Number weight = ((Number)shipmentObject.get("weight"));
-		Number receiptDate = ((Number)shipmentObject.get("receipt_date"));
+		// build a shipment
+		Shipment shipment = new Shipment.Builder()
+				.id(shipmentID)
+				.type(freight)
+				.weight(weight.doubleValue())
+				.date(receiptDate.longValue())
+				.build();
 
+		// add the warehouse
+		warehouseTracker.addWarehouse(warehouse);
 				
-		warehouseTracker.addWarehouse(new Warehouse(warehouseID));
-		
-		warehouseTracker.addShipment(warehouseID, new Shipment(shipmentID
-															 , freight
-															 , weight.doubleValue()
-															 , receiptDate.longValue()));
-		
-		// This is to see results, it will be removed.
-		warehouseTracker.printWarehouseDetails(warehouseID);
-		System.out.println("\n****************************************");		
+		// add the shipment to the warehouse
+		warehouseTracker.addShipment(warehouseID, shipment);
 	}
-	
-	
-	
-	
-	
 }
--- src/com/generic/tracker/WarehouseTracker.java
+++ src/com/generic/tracker/WarehouseTracker.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import com.generic.model.Shipment;
 import com.generic.model.Warehouse;
+
 /**
  * This class will be responsible for tracking
  * a collection of warehouses and the shipments
@@ -14,24 +15,25 @@ import com.generic.model.Warehouse;
  * adding shipment, printing warehouse details,
  * enabling and disabled the freight receipt of a warehouse.
  * 
- * @author Seyi Ola
+ * @author GENERIC TEAM
  *
  */
-public class WarehouseTracker {
+public final class WarehouseTracker {
+	private static WarehouseTracker warehouseTracker;
 	
+	// Stores a collection of warehouses mapped by their id 
+	private Map<Integer, Warehouse> warehouses;
+
+	// private constructor
+	private WarehouseTracker() {}
 	
-	private static WarehouseTracker warehouseTracker = new WarehouseTracker();
-	private Map<Integer, Warehouse> warehouses; // Stores a collection of warehouses
-											    // mapped by their id 
-	
-	private WarehouseTracker()
-	{
-		// Store the warehouse
-		warehouses = new HashMap<>();	
-	}
-	
-	public static WarehouseTracker getInstance()
-	{
+	public static WarehouseTracker getInstance() {
+		if (warehouseTracker == null) {
+			synchronized(WarehouseTracker.class) {
+				warehouseTracker = new WarehouseTracker();
+				warehouseTracker.warehouses = new HashMap<>();
+			}
+		}
 		return warehouseTracker;
 	}
 	
@@ -40,31 +42,41 @@ public class WarehouseTracker {
 	 * Adds a new warehouse to the warehouse collection.
 	 * If the warehouse already exists, we return false.
 	 * @param  mWarehouse warehouse to add.
-	 * @return true if add was successful,
-	 * 		   false if add wasn't.
+	 * @return true if add successful, false if not
 	 */
-	public boolean addWarehouse(Warehouse mWarehouse) 
-	{
-		boolean added = false;
-		if(warehouses.get(mWarehouse.getWarehouseID()) == null)
-		{
+	public boolean addWarehouse(Warehouse mWarehouse) {
+		if(warehouses.get(mWarehouse.getWarehouseID()) == null) {
 			warehouses.put(mWarehouse.getWarehouseID(), mWarehouse);
-			added = true;
+			return true;
 		}
-		return added;
+		return false;
 	}
 	
-	
-	public boolean addShipment(int warehouseID, Shipment mShipment)
-	{
+	/**
+	 * Adds a shipment
+	 * @param warehouseID warehouseID
+	 * @param mShipment the shipment
+	 * @return true if add successful, false if not
+	 */
+	public boolean addShipment(int warehouseID, Shipment mShipment) {
 		Warehouse theWarehouse = warehouses.get(warehouseID);
-		
-		boolean add = (theWarehouse != null && theWarehouse.receivingFreight());
-		if (add) {theWarehouse.addShipment(mShipment);}
-		
-		return add;
+		if (theWarehouse != null && theWarehouse.receivingFreight()) {
+			theWarehouse.addShipment(mShipment);
+			return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Adds a shipment
+	 * @param theWarehouse warehouse to add 
+	 * 		  the shipment to
+	 * @param mShipment shipment to add to warehouse.
+	 * @return true if add successful, false if not. 
+	 */
+	public boolean addShipment(Warehouse theWarehouse, Shipment mShipment) {
+		return addShipment(theWarehouse.getWarehouseID(), mShipment);
+	}
 	
 	/**
 	 * Enables a freight receipt in 
@@ -73,97 +85,88 @@ public class WarehouseTracker {
 	 * @return true if freight was successfully
 	 *         enabled, false if not.
 	 */
-	
-	 public boolean enableFreight(int warehouseID)
-	 {
-		 
+	 public boolean enableFreight(int warehouseID) {
 		 Warehouse theWarehouse = warehouses.get(warehouseID);
-
-		 boolean enabled = (theWarehouse != null);
-		 
-		 if (enabled) {theWarehouse.enableFreight();}
-			
-		 return enabled;
+		 if (theWarehouse != null) {
+			 theWarehouse.enableFreight();
+			 return true;
+		}
+		return false;
 	 }
-	 
-
+	
 	 /**
 	  * Disables freight receipt in a warehouse
 	  * @param warehouseID warehouse id
 	  * @return true if freight was successfully
 	  *         disabled, false if not.
 	  */
-	
 	public boolean endFreight(int warehouseID) {
 		Warehouse theWarehouse = warehouses.get(warehouseID);
-
-		boolean disabled = (theWarehouse != null);
-		if (disabled) {theWarehouse.disableFreight();}
-		
-		return disabled;
+		if (theWarehouse != null) {
+			theWarehouse.disableFreight();
+			return true;
+		}
+		return false;
 	}
-	
+
 	/**
 	 * Checks if we have an empty collection of warehouses.
 	 * @return true if empty, false if not.
 	 */
-	public boolean isEmpty()
-	{
+	public boolean isEmpty() {
 		return warehouses.size() == 0;
 	}
-	
-	
+
 	/**
 	 * Getter for shipments size for a specified warehouse
 	 * @return the size of the warehouse, -1 if
 	 * 		   warehouse doesn't exist
 	 */
-	public int getWarehouseShipmentsSize(int warehouseID)
-	{
+	public int getWarehouseShipmentsSize(int warehouseID) {
 		Warehouse theWarehouse = warehouses.get(warehouseID);
 		return (theWarehouse != null ? theWarehouse.getShipmentSize() : -1);
-		
 	}
-	
-	
-	
+
 	/**
 	 * Exports a warehouse object to JSON file
 	 * @param warehouseID warehouse id
 	 * @param destPath path to write file to
 	 * @return true if warehouse exists, false if not
 	 */
-	public boolean exportWarehouseToJSON (int warehouseID) // What if the warehouse is empty?
-	{
-		
+	public boolean exportWarehouseToJSON (int warehouseID) {
 		Warehouse theWarehouse = warehouses.get(warehouseID);
-		
 		boolean exists = (theWarehouse != null);
-		
-		if (exists){ theWarehouse.exportToJSON(); }
-		
+		if (exists) {
+			theWarehouse.exportToJSON();
+		}
 		return exists;
+	}
+	
+	/**
+	 * Checks if a warehouse exists
+	 * @param warehouseID warehouse id
+	 * @return true if it does, false if not.
+	 */
+	public boolean warehouseExists (int warehouseID) {
+		return (warehouses.get(warehouseID) != null);
 	}
 	
 	/**
 	 * Prints information about a warehouse for user to see.
 	 * @param warehouseID the warehouse id number.
 	 */
-	public void printWarehouseDetails(int warehouseID) 
-	{
+	public void printWarehouseDetails(int warehouseID) {
 		Warehouse theWarehouse = warehouses.get(warehouseID);
 		
-		if (theWarehouse == null) 
-		{
-			System.out.println("Warehouse with ID: " + warehouseID + " doesn't exist");
-		}else 
-		{
+		try {
 			System.out.println(theWarehouse.toString());
+		} catch (NullPointerException e) {
+			throw new IllegalArgumentException("** Warehouse with ID " + warehouseID + " does not exist");
 		}
-
 	}
 	
-	
-	
-
+	public void printAll() {
+		
+		warehouses.forEach((k, v) -> printWarehouseDetails(k));
+	}
 }
